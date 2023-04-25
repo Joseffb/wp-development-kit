03b133c05a2bf26d3ca40f7f7bc7f1dde38b1333
Joseff Betancourt <jbetancourt@mla.org>
2023-04-25 11:26:30 -0400

    Add wordpress indexing registration for Elastic Search and SOlr Search.

--- library/AWS_Elastic_Search_Provider.php
+++ library/AWS_Elastic_Search_Provider.php
@@ -4,9 +4,11 @@ use WDK\Search;
 use WDK\WP_Search_Provider;
 
 class AWS_Elastic_Search_Provider extends WP_Search_Provider {
+
+
     private $es_client;
-    private mixed $index;
-    private mixed $type;
+    private $index;
+    private $doc_type;
 
     public function __construct( $args = array() ) {
         $config = [
@@ -15,38 +17,203 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
             'secret_key' => defined('AWS_SECRET_KEY') ? constant('AWS_SECRET_KEY') : 'YourSecretKeyHere',
             'endpoint' => defined('AWS_ELASTIC_ENDPOINT') ? constant('AWS_ELASTIC_ENDPOINT') : 'https://search-your-instance-id.us-east-1.es.amazonaws.com',
             'index' => defined('AWS_ELASTIC_INDEX') ? constant('AWS_ELASTIC_INDEX') : 'your_index_name',
-            'type' => defined('AWS_ELASTIC_TYPE') ? constant('AWS_ELASTIC_TYPE') : '_doc',
+            'doc_type' => defined('AWS_ELASTIC_DOC_TYPE') ? constant('AWS_ELASTIC_DOC_TYPE') : '_doc',
         ];
 
         $args = wp_parse_args( $args, $config );
 
-        $this->es_client = Elasticsearch\ClientBuilder::create()
-            ->setRegion($args['region'])
+        $this->es_client = ClientBuilder::create()
             ->setHosts([$args['endpoint']])
-            ->setConnectionParams([
-                'client' => [
-                    'timeout' => 30,
-                ],
-                'http' => [
-                    'timeout' => 30,
-                ],
-            ])
-            ->setHttpClient(
-                new AwsSigningHttpClient(
-                    new CurlHttpClient(),
-                    new CredentialProvider(
-                        $args['access_key'],
-                        $args['secret_key']
-                    ),
-                    $args['region'],
-                    'es',
-                    new NullLogger()
-                )
-            )
+            ->setHttpClient(new AwsHttpHandler([
+                'region' => $args['region'],
+                'version' => 'latest',
+                'credentials' => new Credentials(
+                    $args['access_key'],
+                    $args['secret_key']
+                ),
+            ]))
             ->build();
 
         $this->index = $args['index'];
-        $this->type = $args['type'];
+        $this->doc_type = $args['doc_type'];
+    }
+    /**
+     * @param $endpoint
+     * @param $index
+     * @param $region
+     * @param $access_key
+     * @param $secret_key
+     * @return void
+     */
+    public static function register_elastic_indexing($endpoint = null, $index = null, $region = null, $access_key = null, $secret_key = null)
+    {
+        $endpoint = $endpoint ?? defined('AWS_ELASTIC_ENDPOINT') ? constant('AWS_ELASTIC_ENDPOINT') : 'https://search-your-instance-id.us-east-1.es.amazonaws.com';
+        $index = $index ?? defined('AWS_ELASTIC_INDEX') ? constant('AWS_ELASTIC_INDEX') : 'your_index_name';
+        $access_key = $access_key ?? defined('AWS_ACCESS_KEY') ? constant('AWS_ACCESS_KEY') : 'YourAccessKeyHere';
+        $secret_key = $secret_key ?? defined('AWS_SECRET_KEY') ? constant('AWS_SECRET_KEY') : 'YourSecretKeyHere';
+
+        $aws_args = [
+            'region' => $region,
+            'access_key' => $access_key,
+            'secret_key' => $secret_key,
+            'endpoint' => $endpoint,
+            'index' => $index,
+        ];
+
+        $provider = new self($aws_args);
+
+        add_action('wp_insert_post', [$provider, 'add_post_to_elastic_search']);
+        add_action('edit_post', [$provider, 'update_post_in_elastic_search']);
+        add_action('delete_post', [$provider, 'delete_post_from_elastic_search']);
+    }
+
+    /**
+     * Adds a post to the ElasticSearch index.
+     *
+     * @param int $post_id The ID of the post to add.
+     * @return array An array with the status, message, and data keys.
+     */
+    public function add_post_to_elastic_search($post_id): array
+    {
+        $response = [
+            'status' => 'error',
+            'message' => '',
+            'data' => []
+        ];
+
+        if (!class_exists('Elasticsearch\Client')) {
+            $response['message'] = 'Elasticsearch client is not installed';
+            return $response;
+        }
+
+        $post = get_post($post_id);
+
+        // Check if the post is of the desired post type
+        $allowed_post_types = get_option('wdk_elastic_post_types');
+        if (empty($allowed_post_types)) {
+            $allowed_post_types = ['post'];
+        } else {
+            $allowed_post_types = is_array($allowed_post_types) ? $allowed_post_types : explode(',', $allowed_post_types);
+        }
+        if (!in_array($post->post_type, $allowed_post_types, true)) {
+            $response['message'] = 'Post is not of the desired post type';
+            return $response;
+        }
+
+        // Create an ElasticSearch document object for the post
+        $doc = $this->create_elastic_document($post);
+
+        // Add any additional fields to the document using the hook
+        $doc = apply_filters('wdk_elastic_doc_fields', $doc, $post);
+
+        // Index the document in ElasticSearch
+        try {
+            $params = [
+                'index' => $this->index,
+                'type' => $this->type,
+                'id' => $post_id,
+                'body' => $doc->getParams()['body'],
+            ];
+            $this->es_client->index($params);
+
+            $response['status'] = 'success';
+            $response['message'] = 'Post successfully added to ElasticSearch';
+            $response['data'] = $doc->getParams()['body'];
+        } catch (\Exception $e) {
+            $response['status'] = 'fail';
+            $response['message'] = 'Error adding post to ElasticSearch: ' . $e->getMessage();
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param $post_id
+     * @return array
+     */
+    public function update_post_in_elastic_search($post_id): array
+    {
+        $response = [
+            'status' => 'error',
+            'message' => '',
+            'data' => []
+        ];
+
+        if (!class_exists('Elasticsearch\ClientBuilder')) {
+            $response['message'] = 'Elasticsearch client is not installed';
+            return $response;
+        }
+
+        $post = get_post($post_id);
+
+        // Check if the post is of the desired post type
+        $allowed_post_types = get_option('wdk_elastic_post_types');
+        if (empty($allowed_post_types)) {
+            $allowed_post_types = ['post'];
+        } else {
+            $allowed_post_types = is_array($allowed_post_types) ? $allowed_post_types : explode(',', $allowed_post_types);
+        }
+        if (!in_array($post->post_type, $allowed_post_types, true)) {
+            $response['message'] = 'Post is not of the desired post type';
+            return $response;
+        }
+
+        // Create an ElasticSearch document object for the post
+        $doc = $this->create_elastic_search_document($post);
+
+        // Update the corresponding record in ElasticSearch
+        try {
+            $params = [
+                'index' => $this->index,
+                'type' => $this->type,
+                'id' => $post_id,
+                'body' => [
+                    'doc' => $doc->getParams()
+                ]
+            ];
+            $this->es_client->update($params);
+
+            $response['status'] = 'success';
+            $response['message'] = 'Post successfully updated in ElasticSearch';
+            $response['data'] = $doc->getParams();
+        } catch (\Exception $e) {
+            $response['status'] = 'fail';
+            $response['message'] = 'Error updating post in ElasticSearch: ' . $e->getMessage();
+        }
+
+        return $response;
+    }
+
+    /**
+     * Delete a post from Elasticsearch index.
+     *
+     * @param int $post_id ID of the post to be deleted.
+     * @return array An array with status, message, and data keys.
+     */
+    public function delete_post_from_elastic_search($post_id): array
+    {
+        $response = [
+            'status' => 'error',
+            'message' => '',
+            'data' => []
+        ];
+
+        $params = [
+            'index' => $this->index,
+            'type' => $this->type,
+            'id' => $post_id
+        ];
+
+        try {
+            $this->es_client->delete($params);
+            $response['status'] = 'success';
+            $response['message'] = 'Post successfully deleted from Elasticsearch index';
+            $response['data'] = ['post_id' => $post_id];
+        } catch (\Exception $e) {
+            $response['message'] = 'Error deleting post from Elasticsearch index: ' . $e->getMessage();
+        }
+
+        return $response;
     }
 
     public function search( string $query, ?array $args = array() ) {
--- library/Solr_Search_Provider.php
+++ library/Solr_Search_Provider.php
@@ -1,33 +1,234 @@
 <?php
+
 namespace WDK;
+
 use Solarium\Client;
 use Solarium\QueryType\Select\Query\Query;
 
 class Solr_Search_Provider extends WP_Search_Provider
 {
 
-    private mixed $solr_url;
-    private mixed $solr_core;
+    private string $solr_url = 'localhost';
+    private ?string $solr_core = '/solr/mycore';
+    private int $solr_port = 8983;
     private SolrClient $solr_client;
 
-    public function __construct( ?string $solr_url = null, ?string $solr_core = null ) {
+    public function __construct(?string $solr_url = null, ?string $solr_core = null, ?int $solr_port = null)
+    {
         // Set default values if parameters are empty
-        $this->solr_url = $solr_url ?? defined('SOLR_URL')?constant('SOLR_URL'):'https://localhost/solr';
-        $this->solr_core = $solr_core ?? defined('SOLR_CORE')?constant('SOLR_CORE'):'index';
+        $this->solr_url = $solr_url ?? defined('SOLR_URL') ? constant('SOLR_URL') : $this->solr_url;
+        $this->solr_core = $solr_core ?? defined('SOLR_CORE') ? constant('SOLR_CORE') : $this->solr_core;
+        $this->solr_port = $solr_port??$this->solr_port;
 
         // Connect to Solr
         $this->solr_client = new SolrClient([
             'hostname' => $this->solr_url,
-            'port' => defined('SOLR_PORT')?constant('SOLR_PORT'):8983,
+            'port' => $this->solr_port,
             'path' => $this->solr_core,
         ]);
     }
 
-    public function search( string $query, ?array $args = [] ) {
+    public static function register_solr_indexing($hostname = null, $port = null, $path = null)
+    {
+        $port = $port ?? defined('SOLR_PORT') ? constant('SOLR_PORT') : 8983;
+        $hostname = $hostname ?? defined('SOLR_URL') ? constant('SOLR_URL') : 'localhost';
+        $path = $path ?? defined('SOLR_CORE') ? constant('SOLR_CORE') : '/solr/mycore';
+
+        $solr = new self($hostname, $path, $port);
+        add_action('wp_insert_post', [$solr, 'add_post_to_solr']);
+        add_action('edit_post', [$solr, 'update_post_in_solr']);
+        add_action('delete_post', [$solr, 'delete_post_from_solr']);
+    }
+
+    /**
+     * @param $post_id
+     * @return array
+     */
+    public function add_post_to_solr($post_id): array
+    {
+        $response = [
+            'status' => 'error',
+            'message' => '',
+            'data' => []
+        ];
+
+        if (!class_exists('Solarium\Client')) {
+            $response['message'] = 'Solarium client is not installed';
+            return $response;
+        }
+
+        $post = get_post($post_id);
+
+        // Check if the post is of the desired post type
+        $allowed_post_types = get_option('wdk_solr_post_types');
+        if (empty($allowed_post_types)) {
+            $allowed_post_types = ['post'];
+        } else {
+            $allowed_post_types = is_array($allowed_post_types) ? $allowed_post_types : explode(',', $allowed_post_types);
+        }
+        if (!in_array($post->post_type, $allowed_post_types, true)) {
+            $response['message'] = 'Post is not of the desired post type';
+            return $response;
+        }
+
+        // Create a Solr document object for the post
+        $doc = $this->create_solr_document($post);
+
+        // Add any additional fields to the document using the hook
+        $doc = apply_filters('wdk_solr_doc_fields', $doc, $post);
+
+        // Add the document to Solr
+        try {
+            $this->solr_client->addDocument($doc);
+            $this->solr_client->commit();
+
+            $response['status'] = 'success';
+            $response['message'] = 'Post successfully added to Solr';
+            $response['data'] = $doc->getFieldValues();
+        } catch (\RuntimeException $e) {
+            $response['status'] = 'fail';
+            $response['message'] = 'Error adding post to Solr: ' . $e->getMessage();
+        }
+
+        return $response;
+    }
+
+    /**
+     * Update a post in Solr index.
+     *
+     * @param int $post_id ID of the post to be updated.
+     * @return array An array with status, message, and data keys.
+     */
+    public function update_post_in_solr($post_id): array
+    {
+        $response = [
+            'status' => 'error',
+            'message' => '',
+            'data' => []
+        ];
+
+        if (!class_exists('Solarium\Client')) {
+            $response['message'] = 'Solarium client is not installed';
+            return $response;
+        }
+
+        $post = get_post($post_id);
+
+        // Check if the post is of the desired post type
+        $allowed_post_types = get_option('wdk_solr_post_types');
+        if (empty($allowed_post_types)) {
+            $allowed_post_types = ['post'];
+        } else {
+            $allowed_post_types = is_array($allowed_post_types) ? $allowed_post_types : explode(',', $allowed_post_types);
+        }
+        if (!in_array($post->post_type, $allowed_post_types, true)) {
+            $response['message'] = 'Post is not of the desired post type';
+            return $response;
+        }
+
+        // Create a Solr document object for the post
+        $doc = $this->create_solr_document($post);
+
+        // Update the corresponding record in Solr
+        try {
+            $this->solr_client->addDocument($doc);
+            $this->solr_client->commit();
+
+            $response['status'] = 'success';
+            $response['message'] = 'Post successfully updated in Solr';
+            $response['data'] = $doc->getFieldValues();
+        } catch (\RuntimeException $e) {
+            $response['status'] = 'fail';
+            $response['message'] = 'Error updating post in Solr: ' . $e->getMessage();
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param $post_id
+     * @return array
+     */
+    public function delete_post_from_solr($post_id): array
+    {
+        $response = [
+            'status' => 'error',
+            'message' => '',
+            'data' => []
+        ];
+
+        if (!class_exists('Solarium\Client')) {
+            $response['message'] = 'Solarium client is not installed';
+            return $response;
+        }
+
+        $post = get_post($post_id);
+
+        // Check if the post is of the desired post type
+        $allowed_post_types = get_option('wdk_solr_post_types');
+        if (empty($allowed_post_types)) {
+            $allowed_post_types = ['post'];
+        } else {
+            $allowed_post_types = is_array($allowed_post_types) ? $allowed_post_types : explode(',', $allowed_post_types);
+        }
+        if (!in_array($post->post_type, $allowed_post_types, true)) {
+            $response['message'] = 'Post is not of the desired post type';
+            return $response;
+        }
+
+        // Delete the corresponding record from Solr
+        try {
+            $query = "id:$post_id";
+            $this->solr_client->deleteByQuery($query);
+            $this->solr_client->commit();
+
+            $response['status'] = 'success';
+            $response['message'] = 'Post successfully deleted from Solr';
+            $response['data'] = ['post_id' => $post_id];
+        } catch (\RuntimeException $e) {
+            $response['status'] = 'fail';
+            $response['message'] = 'Error deleting post from Solr: ' . $e->getMessage();
+        }
+
+        return $response;
+    }
+    /**usage:
+     *
+    add_filter('wdk_solr_doc_fields', 'my_custom_solr_fields', 10, 2);
+
+    function my_custom_solr_fields($doc, $post) {
+        $doc->addField('custom_field', 'Custom Value');
+        return $doc;
+    }
+     * @param $post
+     * @return SolrInputDocument
+     */
+    private function create_solr_document($post = null): SolrInputDocument
+    {
+        if (empty($post)) {
+            throw new \InvalidArgumentException('Post parameter cannot be null.');
+        }
+
+        $doc = new SolrInputDocument();
+        $doc->addField('id', $post->ID);
+        $doc->addField('title', $post->post_title);
+        $doc->addField('content', $post->post_content);
+
+        // Allow users to add or modify fields using a filter hook
+        return apply_filters('wdk_solr_doc_fields', $doc, $post);
+    }
+
+    /**
+     * @param string $query
+     * @param array|null $args
+     * @return \WP_Query
+     */
+    public function search(string $query, ?array $args = []): \WP_Query
+    {
         // Set default values if parameters are empty
-        $rows = $args['rows'] ?? defined('SOLR_ROWS')?constant('SOLR_ROWS'):1;
-        $start = $args['start'] ?? defined('SOLR_START')?constant('SOLR_START'):0;
-        $sort = $args['sort'] ?? defined('SOLR_SORT')?constant('SOLR_SORT'):'Asc';
+        $rows = $args['rows'] ?? defined('SOLR_ROWS') ? constant('SOLR_ROWS') : 1;
+        $start = $args['start'] ?? defined('SOLR_START') ? constant('SOLR_START') : 0;
+        $sort = $args['sort'] ?? defined('SOLR_SORT') ? constant('SOLR_SORT') : 'Asc';
 
         // Prepare query
         $query = new SolrQuery($query);
@@ -58,20 +259,20 @@ class Solr_Search_Provider extends WP_Search_Provider
 }
 
 /**
-
-// Create a new Search manager with the Solr search provider
-$search_manager = new Search('Solr_Search_Provider');
-
-// Search for posts with the query 'WordPress'
-$results = $search_manager->search('wordpress');
-
-// Loop through the results and output the post titles
-if ($results->have_posts()) {
-while ($results->have_posts()) {
-$results->the_post();
-echo get_the_title() . '<br>';
-}
-} else {
-echo 'No results found.';
-}
+ *
+ * // Create a new Search manager with the Solr search provider
+ * $search_manager = new Search('Solr_Search_Provider');
+ *
+ * // Search for posts with the query 'WordPress'
+ * $results = $search_manager->search('wordpress');
+ *
+ * // Loop through the results and output the post titles
+ * if ($results->have_posts()) {
+ * while ($results->have_posts()) {
+ * $results->the_post();
+ * echo get_the_title() . '<br>';
+ * }
+ * } else {
+ * echo 'No results found.';
+ * }
  */
