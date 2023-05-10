424f71ee90270b70c91aaa77f7b143ec99bfd4db
Joseff Betancourt <jbetancourt@mla.org>
2023-05-10 10:07:27 -0400

    Debugged Elastic Search search provider

--- library/AWS_Elastic_Search_Provider.php
+++ library/AWS_Elastic_Search_Provider.php
@@ -1,14 +1,22 @@
 <?php
+
 namespace WDK;
 
-class AWS_Elastic_Search_Provider extends WP_Search_Provider {
+use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\Exception\AuthenticationException;
 
+class AWS_Elastic_Search_Provider extends WP_Search_Provider
+{
 
     private $es_client;
     private $index;
     private $doc_type;
-
-    public function __construct( $args = array() ) {
+    private bool $logged_in=false;
+    /**
+     * @throws AuthenticationException
+     */
+    public function __construct($args = array())
+    {
         $config = [
             'region' => defined('AWS_REGION') ? constant('AWS_REGION') : 'us-east-1',
             'access_key' => defined('AWS_ACCESS_KEY') ? constant('AWS_ACCESS_KEY') : 'YourAccessKeyHere',
@@ -18,23 +26,24 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
             'doc_type' => defined('AWS_ELASTIC_DOC_TYPE') ? constant('AWS_ELASTIC_DOC_TYPE') : '_doc',
         ];
 
-        $args = wp_parse_args( $args, $config );
+        $args = wp_parse_args($args, $config);
 
-        $this->es_client = ClientBuilder::create()
-            ->setHosts([$args['endpoint']])
-            ->setHttpClient(new AwsHttpHandler([
-                'region' => $args['region'],
-                'version' => 'latest',
-                'credentials' => new Credentials(
-                    $args['access_key'],
-                    $args['secret_key']
-                ),
-            ]))
-            ->build();
+        $args = wp_parse_args($args, $config);
+
+        try {
+            $this->es_client = ClientBuilder::create()
+                ->setElasticCloudId($args['endpoint'])
+                ->setApiKey($args['access_key'], $args['secret_key'])
+                ->build();
+            $this->logged_in = true;
+        } catch (\Exception $e) {
+            Utility::Log('no connection');
+        }
 
         $this->index = $args['index'];
         $this->doc_type = $args['doc_type'];
     }
+
     /**
      * @param $endpoint
      * @param $index
@@ -45,6 +54,7 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
      */
     public static function register_elastic_indexing($endpoint = null, $index = null, $region = null, $access_key = null, $secret_key = null)
     {
+
         $endpoint = $endpoint ?? defined('AWS_ELASTIC_ENDPOINT') ? constant('AWS_ELASTIC_ENDPOINT') : 'https://search-your-instance-id.us-east-1.es.amazonaws.com';
         $index = $index ?? defined('AWS_ELASTIC_INDEX') ? constant('AWS_ELASTIC_INDEX') : 'your_index_name';
         $access_key = $access_key ?? defined('AWS_ACCESS_KEY') ? constant('AWS_ACCESS_KEY') : 'YourAccessKeyHere';
@@ -59,36 +69,38 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
         ];
 
         $provider = new self($aws_args);
-
+        if(!$provider->logged_in) {
+            return false;
+        }
         add_action('wp_insert_post', [$provider, 'add_post_to_elastic_search']);
         add_action('edit_post', [$provider, 'update_post_in_elastic_search']);
         add_action('delete_post', [$provider, 'delete_post_from_elastic_search']);
     }
 
-    public static function is_available(): bool
+    public static function is_available($endpoint = null, $index = null, $region = null, $access_key = null, $secret_key = null): bool
     {
-        $config = [
-            'region' => defined('AWS_REGION') ? constant('AWS_REGION') : 'us-east-1',
-            'access_key' => defined('AWS_ACCESS_KEY') ? constant('AWS_ACCESS_KEY') : 'YourAccessKeyHere',
-            'secret_key' => defined('AWS_SECRET_KEY') ? constant('AWS_SECRET_KEY') : 'YourSecretKeyHere',
-            'endpoint' => defined('AWS_ELASTIC_ENDPOINT') ? constant('AWS_ELASTIC_ENDPOINT') : 'https://search-your-instance-id.us-east-1.es.amazonaws.com',
-            'index' => defined('AWS_ELASTIC_INDEX') ? constant('AWS_ELASTIC_INDEX') : 'your_index_name',
-            'doc_type' => defined('AWS_ELASTIC_DOC_TYPE') ? constant('AWS_ELASTIC_DOC_TYPE') : '_doc',
-        ];
+        $endpoint = $endpoint ?? defined('AWS_ELASTIC_ENDPOINT') ? constant('AWS_ELASTIC_ENDPOINT') : 'https://search-your-instance-id.us-east-1.es.amazonaws.com';
+        $index = $index ?? defined('AWS_ELASTIC_INDEX') ? constant('AWS_ELASTIC_INDEX') : 'your_index_name';
+        $access_key = $access_key ?? defined('AWS_ACCESS_KEY') ? constant('AWS_ACCESS_KEY') : 'YourAccessKeyHere';
+        $secret_key = $secret_key ?? defined('AWS_SECRET_KEY') ? constant('AWS_SECRET_KEY') : 'YourSecretKeyHere';
 
+        $aws_args = [
+            'region' => $region,
+            'access_key' => $access_key,
+            'secret_key' => $secret_key,
+            'endpoint' => $endpoint,
+            'index' => $index,
+        ];
+        $provider = new self($aws_args);
+        if(!$provider->logged_in) {
+            return false;
+        }
         // Instantiate the AWS SDK client
-        $client = Elasticsearch\ClientBuilder::create()
-            ->setRegion($config['region'])
-            ->setCredentials([
-                'key' => $config['access_key'],
-                'secret' => $config['secret_key'],
-            ])
-            ->setEndpoint($config['endpoint'])
-            ->build();
+        $client = $provider->es_client;
 
         try {
             // Ping the Elasticsearch instance
-            $response = $client->ping();
+            $response = (bool) $client->ping();
 
             // Check that the response was successful
             if ($response !== true) {
@@ -97,8 +109,8 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
 
             // Check that the Elasticsearch index and doc type exist
             $params = [
-                'index' => $config['index'],
-                'type' => $config['doc_type'],
+                'index' => $provider->index,
+                'type' => $provider->index->doc_type,
             ];
 
             $exists = $client->indices()->existsType($params);
@@ -262,13 +274,14 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
         return $response;
     }
 
-    public function search( string $query, ?array $args = array() ) {
-        $args = wp_parse_args( $args, array(
+    public function search(string $query, ?array $args = array())
+    {
+        $args = wp_parse_args($args, array(
             'size' => 10,
             'from' => 0,
             'sort_field' => 'date',
             'sort_order' => 'desc'
-        ) );
+        ));
 
         $params = [
             'index' => $this->index,
@@ -300,51 +313,51 @@ class AWS_Elastic_Search_Provider extends WP_Search_Provider {
             ],
         ];
 
-        $results = $this->es_client->search( $params );
+        $results = $this->es_client->search($params);
 
         $post_ids = array();
-        if ( isset( $results['hits'] ) && isset( $results['hits']['hits'] ) ) {
-            foreach ( $results['hits']['hits'] as $hit ) {
+        if (isset($results['hits']) && isset($results['hits']['hits'])) {
+            foreach ($results['hits']['hits'] as $hit) {
                 $post_ids[] = $hit['_id'];
             }
         }
 
         $wp_query_args = array(
-            'post_type' => array( 'post', 'page' ),
+            'post_type' => array('post', 'page'),
             'post__in' => $post_ids,
             'orderby' => 'post__in',
             'posts_per_page' => $args['size'],
             'offset' => $args['from']
         );
-        return new \WP_Query( $wp_query_args );
+        return new \WP_Query($wp_query_args);
     }
 }
 
 /**
  * $search_manager = new Search('Elasticsearch_Search_Provider');
-
-$query = 'example search query';
-$args = [
-'posts_per_page' => 10,
-'paged' => get_query_var('paged') ?: 1,
-'meta_query' => [
-[
-'key' => 'example_meta_key',
-'value' => 'example_meta_value',
-'compare' => '='
-]
-]
-];
-
-$results = $search_manager->search($query, $args);
-
-// Output the results
-if ($results->have_posts()) {
-while ($results->have_posts()) {
-$results->the_post();
-// Display the post
-}
-} else {
-// No results found
-}
+ *
+ * $query = 'example search query';
+ * $args = [
+ * 'posts_per_page' => 10,
+ * 'paged' => get_query_var('paged') ?: 1,
+ * 'meta_query' => [
+ * [
+ * 'key' => 'example_meta_key',
+ * 'value' => 'example_meta_value',
+ * 'compare' => '='
+ * ]
+ * ]
+ * ];
+ *
+ * $results = $search_manager->search($query, $args);
+ *
+ * // Output the results
+ * if ($results->have_posts()) {
+ * while ($results->have_posts()) {
+ * $results->the_post();
+ * // Display the post
+ * }
+ * } else {
+ * // No results found
+ * }
  */
